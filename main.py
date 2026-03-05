52b4566ca8f3fb01aaf0e8915e425a7a177432aa
Jackson <Jacksonrudnick32@gmail.com>
2026-03-05 16:01:21 -0600

    getting somewhere

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 from torch.nn import functional as f
 import pickle
 import random
+from tqdm import tqdm
+import numpy as np
 
 # Hyper params
 @dataclass
@@ -14,20 +16,34 @@ class cfg():
     stride = 1
     padding = 2
     num_kernels = 6
+    num_pools = 5
+    batch_size = 10000
+    num_batches = 5
+    mini_batch_size = 1000
+    max_iters = 5000
+    learning_rate = 3e-4
 #############################
 
-def unpickle(file):
+def unpickle(file) -> dict:
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
 def get_batch(split):
     if split == 'train':
-        batch_num = random.randint(1,5)
+        batch_num = random.randint(1,5)        
+        data = unpickle(f'cifar-10-batches-py/data_batch_{batch_num}')
+    else: 
+        data = unpickle(f'cifar-10-batches-py/test_batch')
+
+    indices = random.sample(range(cfg.batch_size), cfg.mini_batch_size)
+
+    xs = torch.tensor(data[b'data'][indices], dtype=torch.float32)
+    xs = xs.view(-1, 3, 32, 32)
+    ys = torch.tensor([data[b'labels'][i] for i in indices], dtype=torch.long)
+
+    return (xs, ys)
         
-        return unpickle(f'cifar-10-batches-py/data_batch_{batch_num}')
-    return unpickle(f'cifar-10-batches-py/test_batch')
-    
 class Kernel(nn.Module):
     """Defines a Kernel/Filter to be applied on a volume"""
     def __init__(self, f: int, d: int, s: int, p: int):
@@ -48,12 +64,14 @@ class Kernel(nn.Module):
         self.stride = s
         self.pad = p
         self.ln = nn.Linear(f*f*d, 1)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         # 32x32x3
         W, H, D = x.shape
+        print(x.shape)
 
-        x = f.pad(x, [self.pad], 'constant')
+        x = f.pad(x, [self.pad, self.pad, self.pad, self.pad], 'constant')
 
         out = torch.tensor(())
         # image p = 2 f = 5
@@ -66,12 +84,15 @@ class Kernel(nn.Module):
                 # aka selecting the data the kernel should intake
                 # concatenate all data to create new activation map which should be size 28x28x1 without padding
                 out = torch.cat((out, self.ln(x[r:r+self.width, c:c+self.width].flatten())))
-        
+
+        # With 2p 5f output is 32x32x1
+        out = out.view(W, H)
+
         return out
-    
+
 class Conv(nn.Module):
     """Defines a Convolutional layer containing a number of Kernels"""
-    def __init__(self, n: int, s: int, f: int, d: int, p: int, do_pooling=True):
+    def __init__(self, n: int, s: int, f: int, d: int, p: int):
         """
         Create a Convolutional Layer
         Layer contains n Kernels with f width height, and d depth
@@ -84,25 +105,71 @@ class Conv(nn.Module):
             p --- Padding of image
         """
         super().__init__()
-        
-        if do_pooling:
-            self.do_pooling = True
-            self.pools = nn.MaxPool3d(kernel_size=f)
-        else:
-            self.do_pooling = False
 
         self.kernels = nn.ModuleList([Kernel(f, d, s, p) for _ in range(n)])
 
     def forward(self, x):
         out = torch.tensor(())
 
+        # for 6 kernels, output should be 32x32x6
         for k in self.kernels:
             out = torch.cat((out, k(x)))
 
         out = f.relu(out)
 
-        if self.do_pooling:
-            out = self.pools(out)
-
         return out
 
+class Net(nn.Module):
+    """Defines the network that combines all the classes above"""
+    def __init__(self, n: int, s: int, f: int, d: int, p: int):
+        """
+        Create the network
+        """
+        super().__init__()
+        
+        self.kernels = nn.ModuleList([Conv(n, f, d, s, p) for _ in range(cfg.num_kernels)])
+        self.pools = nn.ModuleList([nn.MaxPool3d(f) for _ in range(cfg.num_pools)])
+        self.ln = nn.Linear(16, 10)
+
+    def forward(self, x, targets=None):
+        for i in range(len(self.kernels)):
+            x = self.kernels[i](x)
+            if i != 0:
+                x = self.pools[i-1](x)
+
+        # 32x32x6 -> 28x28x6
+        # 28x28x6 -> 24x24x6
+        # 24 -> 20 -> 16
+
+        x = self.ln(x)
+
+        if targets == None:
+            return (x,)
+        else:
+            loss = f.cross_entropy(x, targets)
+
+        return (x, loss)
+
+m = Net(cfg.num_kernels, cfg.stride, cfg.width, cfg.depth, cfg.padding)
+optimizer = torch.optim.Adam(m.parameters(), lr=cfg.learning_rate)
+
+def train():
+    for iter in tqdm(range(cfg.max_iters)):
+        # 1000x3x32x32
+        xs, ys = get_batch('train')
+
+        logits, loss = m(xs, ys)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+def test():
+    xs, ys = get_batch('test')
+
+    logits, loss = m(xs, ys)
+
+    print(f"Test Loss: {loss.item()}")
+
+if __name__ == "__main__":
+    train()
+    # test()
